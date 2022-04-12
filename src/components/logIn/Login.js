a20ec477ca4ba7024b902d1c8dd3902119f0f28c
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-12 17:32:31 +0600

    added update user info function

--- src/components/logIn/Login.js
+++ src/components/logIn/Login.js
@@ -1,14 +1,16 @@
-import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { getAuth, GoogleAuthProvider, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import React, { useState } from 'react';
 import app from '../../firebaseConfig';
 import './Login.css'
 import { useNavigate } from 'react-router-dom';
 
 const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
 
 const Login = () => {
     const navigate = useNavigate()
-    const provider = new GoogleAuthProvider()
+    const [error, setError] = useState('')
+    const [email, setEmail] = useState('')
     // google login
     const googleSignIn = () => {
         signInWithPopup(auth, provider)
@@ -26,15 +28,28 @@ const Login = () => {
               navigate('/')
           })
           .catch((error) => {
-            console.log(error.message);
+              console.log(error.message);
+              setError(error.message)
           });
     }
-    
+    // Password reset email sent!
+    const reserPassword = () => {
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                console.log('pass reset');
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
     return (
         <div className='login-page mx-auto'>
             <form onSubmit={LoginWithPass} className="log-fild">
-                <input  className="py-1 px-2" type="email" name='email'  placeholder='Email' />
-                <input  className="py-1 px-2" type="password" name='password' autoComplete="off" placeholder='Password' />
+                <input onChange={(event) => setEmail(event.target.value)} className="py-1 px-2" type="email" name='email'  placeholder='Email' />
+                <input className="py-1 px-2" type="password" name='password' autoComplete="off" placeholder='Password' />
+                {error && <p>{error}</p>}
+                <p onClick={reserPassword}>Forget Password</p>
                 <button className="bg-warning rounded py-1 px-2" type="submit">Login</button>
             </form>
             <p className='mx-auto p-0 m-0 text-primary'> Or </p>
--- src/components/singup/SignUp.js
+++ src/components/singup/SignUp.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, sendEmailVerification, signInWithPopup, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import app from '../../firebaseConfig';
 
@@ -6,6 +6,7 @@ const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 
 const SignUp = () => {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState({value : '', error : ''})
     const [password, setPassword] = useState({value : '', error : ''})
     const [confirmpassword, setConfirmPassword] = useState({ value: '', error: '' })
@@ -16,7 +17,6 @@ const SignUp = () => {
         if (value === '') {
             return
         }
-
         if (/\S+@\S+\.\S+/.test(value)) {
             setEmail({value : value, error: ''})
         } else {
@@ -24,9 +24,8 @@ const SignUp = () => {
             
         }
     }
+    // check password validity
     const passValidity = (value) => {
-        console.log(value.length)
-
         if (value === '') {
             return
         }
@@ -42,8 +41,7 @@ const SignUp = () => {
             setPassword({ value: '', error: 'Need at least one 8 characters' })
             return;
         }
-        setPassword({ value: value, error: '' }) 
-        console.log(value);
+        setPassword({ value: value, error: '' })
     }
 
     // google sign in
@@ -55,15 +53,29 @@ const SignUp = () => {
     
     // register new user
     const signUpWithPass = (event) => {
+        event.preventDefault()    
+        // Email verification sent!
         
-        console.log('hello');
-
-        event.preventDefault()       
+            
+        
         // create new user 
         if (email.value && password.value) {
             createUserWithEmailAndPassword(auth, email.value, password.value)
                 .then((userCredential) => {
-                console.log(userCredential.user);
+                    // send email validity
+                    sendEmailVerification(auth.currentUser)
+                    .then(() => {
+                        console.log('email sent');
+                    })
+                    .catch((error) => console.log(error.message))
+                    console.log(userCredential.user);
+                    // update user info
+                    updateProfile(auth.currentUser, { displayName: name })
+                        .then(() => {
+                            console.log('added name');
+                        }).catch((error) => {
+                          console.log(error.message);
+                        });
                 })
                 .catch((error) => {
                     const errorMessage = error.message;
@@ -72,17 +84,19 @@ const SignUp = () => {
                 });
         }
     }
-    
+
+
     return (
         <div>
             <div className='login-page mx-auto'>
-                <form onSubmit={signUpWithPass} className="log-fild">  
-                    <input onBlur={(event) => emailValidity(event.target.value)} className="py-1 px-2" type="email" name='email'  placeholder='Email' required />
+                <form  className="log-fild">  
+                    <input onChange={(event) => setName(event.target.value)} className="py-1 px-2" type="text" name='name'  placeholder='Name' required />
+                    <input onChange={(event) => emailValidity(event.target.value)} className="py-1 px-2" type="email" name='email'  placeholder='Email' required />
                     { email?.error && <p>{email?.error}</p>}
-                    <input onBlur={(event) => passValidity(event.target.value)} className="py-1 px-2" type="password" name='password' autoComplete="off" placeholder='Password' required />
+                    <input onChange={(event) => passValidity(event.target.value)} className="py-1 px-2" type="password" name='password' autoComplete="off" placeholder='Password' required />
                     { password?.error && <p>{password?.error}</p>}
                     {/* <input  className="py-1 px-2" type="password" name='confirmPassword' autoComplete="off" placeholder='Confirm Password' /> */}
-                    <button className="bg-warning rounded py-1 px-2" type="submit">Sign Up</button>
+                    <button onClick={signUpWithPass} className="bg-warning rounded py-1 px-2" type="submit">Sign Up</button>
                 </form>
                 <p className='mx-auto p-0 m-0 text-primary'>Or</p>
                 <button onClick={googleSignIn} className="bg-warning rounded py-1 px-2" type="submit">Google LogIn</button>
